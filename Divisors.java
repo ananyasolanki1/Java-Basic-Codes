499ec7c12b98426e6878ed6463570a69c0d36abb
ananyasolanki1 <ananya.solanki10@gmail.com>
2025-10-11 23:30:06 +0530

    Final Divisors.java

--- Divisors.java
+++ Divisors.java
@@ -1,22 +1,62 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Divisors {
     public static void main(String[] args) {
-        int num = 36;
-        System.out.print("[");
-        divs(num);
-        System.out.print("]");
+        int num = 15;
+        // System.out.print("[");  // Brute force 1
+        // divs(num);
+        // System.out.print("]");
+        
+        // int[] narr = new int[num];   // Brure force 2
+        // narr = divs(num);
+        // for (int i = 0; i < num; i++) {
+        //     if (narr[i] != 0) {
+        //         System.out.println(narr[i]);
+        //     }
+        // }
+
+        System.out.println(divs(num));   // Optimal  
     }
     
-    public static void divs(int n) {    // brute force  TC: O(n)  SC: O(n)
-        int arr[] = new int[n];
-        int cnt = 0;
-        for (int i = 1; i <= n; i++) {
+    // public static void divs(int n) {   // Brute force 1 - TC: O(n)  SC: O(d-divisors) directly printing the array
+    //     int arr[] = new int[n];
+    //     int cnt = 0;
+    //     for (int i = 1; i <= n; i++) {
+    //         if (n % i == 0) {
+    //             arr[cnt] = i;
+    //             cnt++;
+    //         }
+    //     }
+    //     for (int i = 0; i < cnt; i++) {
+    //         System.out.print(arr[i] + ", ");
+    //     }
+    // }
+    
+    // public static int[] divs(int n) {    // Brute force 2 - TC: O(n)  SC: O(d) returning the array
+    //     int arr[] = new int[n];
+    //     int cnt = 0;
+    //     for (int i = 1; i <= n; i++) {
+    //         if (n % i == 0) {
+    //             arr[cnt++] = i;
+    //         }
+    //     }
+    //     return arr;
+    // }
+    
+    static ArrayList<Integer> divs(int n) {                 // optimal   TC: O(√n)   SC: O(d) checking till n sqr
+        ArrayList<Integer> divisors = new ArrayList<>();
+        int sqn = (int) Math.sqrt(n);
+        for (int i = 1; i <= sqn; i++) {
             if (n % i == 0) {
-                arr[cnt] = i;
-                cnt++;
+                divisors.add(i);
+                if (i != n/i) {
+                    divisors.add(n/i);
+                }
             }
-        }
-        for (int i = 0; i < cnt; i++) {
-            System.out.print(arr[i] + ", ");
-        }
+        } 
+        Collections.sort(divisors);
+        return divisors; 
     }
+    
 }
