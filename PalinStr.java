adf9736753e2bdf7268651220fe6f4c4f11b0d73
ananyasolanki1 <ananya.solanki10@gmail.com>
2025-10-29 22:52:36 +0530

    Final PalinStr.java

--- PalinStr.java
+++ PalinStr.java
@@ -1,10 +1,10 @@
 public class PalinStr { // String is palindrome or not
     public static void main(String[] args) {
-        // String s = "A man, a plan, a canal: Panama";
-        // boolean ans = palin(s);
+        String s = "A man, a plan, a canal: Panama";
+        boolean ans = palin(s);
 
-        String s = "anarna";
-        boolean ans = palin(0, s);
+        // String s = "anarna";
+        // boolean ans = palin(0, s);
         if (ans) {
             System.out.println();
             System.out.println("Palindrome");
@@ -14,7 +14,7 @@ public class PalinStr { // String is palindrome or not
         }
     }
 
-    // public static boolean palin(String str) {            // Method 1:   TC: O(n/2) => O(n)    SC:O(1)
+    // public static boolean palin(String str) {            // Method 1:   TC: O(n²)   SC: O(n)
     //     int left = 0;                                    // Defining left and right
     //     int right = str.length() - 1;
     //     while (left < right) {
@@ -26,8 +26,8 @@ public class PalinStr { // String is palindrome or not
     // }
 
 
-    // public static boolean palin(String str) {             // Method 2:  TC: O(n/2) => O(n)   SC:O(1)
-    //     int ln = str.length();                            // without left and right pointers
+    // public static boolean palin(String str) {             // Method 2:  TC: O(n²)   SC: O(n)
+    //     int ln = str.length();                            // without left and right pointers    
     //     for (int i = 0; i < ln/2; i++) {
     //         if (str.toLowerCase().charAt(i) != str.toLowerCase().charAt(ln - 1 - i)) {
     //         return false;
@@ -37,9 +37,9 @@ public class PalinStr { // String is palindrome or not
     // }
 
 
-    // public static boolean palin(String str) {              // Method 3:  TC: O(n/2) => O(n)   SC: O(1)   
+    // public static boolean palin(String str) {              // Method 3:  TC: O(n²)   SC: O(n)    
     //     int ln = str.length();                             // considering only alphabets and numbers
-    //     int left = 0;
+    //     int left = 0;                                      // LEETCODE - TIME LIMIT EXCEEDED as toLowerCase() creates new string everytime
     //     int right = ln - 1;
     //     while (left < right) {
     //         if (!Character.isLetterOrDigit(str.charAt(left))) {
@@ -54,13 +54,57 @@ public class PalinStr { // String is palindrome or not
     // }
 
 
-    public static boolean palin(int i, String str) {            // Method 4: Recursion   TC: O(n/2) => O(n)   SC: O(n/2) as each call is stored in stack
-        if (i >= str.length()/2) {
-            return true;
+    // public static boolean palin(int i, String str) {            // Method 4: Recursion   TC: O(n)   SC: O(n)
+    //     if (i >= str.length()/2) {
+    //         return true;
+    //     }
+    //     if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+    //         return false;
+    //     }
+    //     return palin(i + 1, str);
+    // }
+
+    
+    // public static boolean palin(String str) {                        // Method 5: Regular Expression, replacing   TC: O(n)   SC: O(n)
+    //     str = str.trim().toLowerCase().replaceAll("[^a-z0-9]", "");
+    //     int left = 0;
+    //     int right = str.length() - 1;
+    //     while (left < right) {
+    //         if (str.charAt(left++) != str.charAt(right--) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+
+
+    public static boolean palin(String str) {                        // Method 6: MOST OPTIMIZED   TC: O(n)   SC: O(1)
+        int left = 0;                                                // BEST ONE
+        int right = str.length() - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(str.charAt(left))) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(str.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(str.charAt(left++)) != Character.toLowerCase(str.charAt(right--))) {
+                return false;
+            }
         }
-        if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-            return false;
-        }
-        return palin(i + 1, str);
+        return true;
     }
+
+
+    // public static boolean palin(String str) {                       // Method 7: lowercase done once outside loop   TC: O(n)   SC: O(n) total / O(1) auxiliary
+    //     String lower = str.toLowerCase();
+    //     int left = 0;
+    //     int right = lower.length() - 1;
+    //     while (left < right) {
+    //         if (lower.charAt(left++) != lower.charAt(right--)) {
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
