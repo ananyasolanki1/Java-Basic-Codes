4ec7b41166afa91c99c1a22b264f0cdfda9e56db
ananyasolanki1 <ananya.solanki10@gmail.com>
2025-11-29 08:45:08 +0530

    finalized BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,23 +2,27 @@ import java.util.*;
 
 class BubbleSort {
     public static void main(String[] args) {
-        // System.out.println("Try programiz.pro");
         int[] arr = {13, 9, 42, 20, 56, 12};
         bubbleSort(arr);
     }
     
-    static void bubbleSort(int[] arr) {
-        int n = arr.length;
+    static void bubbleSort(int[] arr) {             // swapped does TC: O(n) in best case, otherwise its TC: O(n²) in all case. SC: O(1) with or without swapped
+        int n = arr.length;   // n = 6
         int temp;
-        for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < n - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+        for (int i = 0; i < n - 1; i++) {           // compare till last elem, then till 2nd last, then till 3rd last....
+            boolean swapped = false;                // for each pass, resets the swapped to false
+            for (int j = 0; j < n - 1 - i; j++) {   
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;                 // mark that a swap happened in this pass (array was not fully sorted), so checks next pass
                 }
             }
-            System.out.println(Arrays.toString(arr));
+            if (!swapped) {                          // if no swapped happened in a pass, means its sorted, so exit
+                break;
+            }
         }
+        System.out.println(Arrays.toString(arr));
     }
 }
